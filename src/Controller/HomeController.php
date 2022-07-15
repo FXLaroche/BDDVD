486d20dac490ccfd8d5ff2424713bd8673027676
FXLaroche <fx.laroche@gmail.com>
2022-07-15 10:56:31 +0200

    login and signin remade

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\FilmRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +12,14 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(FilmRepository $filmRepository): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('film_list', ['id' => $this->getUser()->getId()]);
         }
-        return $this->render('home/index.html.twig', ['title' => '']);
+        return $this->render('home/index.html.twig', [
+            'title' => '',
+            'filmCount' => count($filmRepository->findAll()),
+            ]);
     }
 }
